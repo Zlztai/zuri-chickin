ba231bb95a5a83b807adf5964eef547f45427e9e
Jokerjin9 <129597814+Jokerjin9@users.noreply.github.com>
2023-04-19 11:38:58 +0800

    Update suyunChinck.py

--- suyunChinck.py
+++ suyunChinck.py
@@ -5,13 +5,15 @@ import time
 username = sys.argv[1] # 登录账号
 password = sys.argv[2] # 登录密码
 
-@retry(stop_max_attempt_number=5)
 def hupu():
     try:
         driver = get_web_driver()
         driver.get("https://yooo.one/auth/login")
+        time.sleep(3)
         driver.find_element('xpath', '//*[@id="email"]').send_keys(username)
+        time.sleep(2)
         driver.find_element('xpath', '//*[@id="password"]').send_keys(password)
+        time.sleep(2)
         driver.find_element('xpath', '//*[@id="login_submit"]').click()
         time.sleep(5)
         driver.find_element('xpath', '//*[@id="checkin"]').click()

c6c75ea7b8696b4b4c1304fcc96e1f64cf1fe19a
Jokerjin9 <129597814+Jokerjin9@users.noreply.github.com>
2023-04-19 13:43:10 +0800

    Update Method.py

--- Method.py
+++ Method.py
@@ -28,6 +28,37 @@ def get_web_driver():
     return driver
 
 
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
+def mail(txt,my_sender,my_user,my_pass):
+    
+    ret = True
+    try:
+        msg = MIMEText(txt, 'plain', 'utf-8')  # 填写邮件内容
+        msg['From'] = formataddr(["tracy", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To'] = formataddr(["test", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject'] = "极速云签到"  # 邮件的主题，也可以说是标题
+
+        server = smtplib.SMTP_SSL("smtp.qq.com", 465)  # 发件人邮箱中的SMTP服务器
+        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱授权码
+        server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()  # 关闭连接
+    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+        ret = False
+    return ret
+
+
+ret = mail()
+if ret:
+    print("邮件发送成功")
+else:
+    print("邮件发送失败")
+
+
+
+
 
 #验证码处理
 # import ddddocr

a38ff7d0d55fc71448ad24cc397fbc5d9e5d136d
Zlztai <121115645+Zlztai@users.noreply.github.com>
2023-04-25 09:55:57 +0800

    Update suyunChinck.py

--- suyunChinck.py
+++ suyunChinck.py
@@ -12,13 +12,13 @@ my_user = sys.argv[5]  # 收件人邮箱账号
 def suyunChinck():
     try:
         driver = get_web_driver()
-        driver.get("https://yooo.one/auth/login")
+        driver.get("https://zhuri.cc/auth/login")
         time.sleep(3)
         driver.find_element('xpath', '//*[@id="email"]').send_keys(username)
         time.sleep(2)
         driver.find_element('xpath', '//*[@id="password"]').send_keys(password)
         time.sleep(2)
-        driver.find_element('xpath', '//*[@id="login_submit"]').click()
+        driver.find_element('xpath', '//*[@id="login-form"]/button').click()
         time.sleep(5)
         try:
             #签到
@@ -29,20 +29,20 @@ def suyunChinck():
         finally: 
             print('按钮不可点击，已签到？？')
             #获取元素值
-            result = driver.find_element('xpath', '//*[@id="kt_subheader"]/div/div[2]/a').text
+#             result = driver.find_element('xpath', '//*[@id="kt_subheader"]/div/div[2]/a').text
            
-    except:
-        pass
-    finally:
-        driver.quit()
-    time.sleep(3)
-    print(result)
-    #发送邮件
-    ret = mail(result,my_sender,my_user,my_pass)
-    if ret:
-        print("邮件发送成功")
-    else:
-        print("邮件发送失败")
-    return result
+#     except:
+#         pass
+#     finally:
+#         driver.quit()
+#     time.sleep(3)
+#     print(result)
+#     #发送邮件
+#     ret = mail(result,my_sender,my_user,my_pass)
+#     if ret:
+#         print("邮件发送成功")
+#     else:
+#         print("邮件发送失败")
+#     return result
 if __name__ == '__main__':
     suyunChinck()
